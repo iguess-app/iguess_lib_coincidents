b3bdf2fa5958e5ca33eab2253d1b5813ffac206d
Lucas Alves <lucashalves@live.com>
2017-04-23 20:39:40 -0300

    Added guessLeagueStatus Util

--- Utils/export.js
+++ Utils/export.js
@@ -3,11 +3,13 @@ const passwordUtils = require('./passwordUtils.js');
 const profileUtils = require('./profileUtils.js');
 const queryUtils = require('./queryUtils.js');
 const statusUtils = require('./statusUtils.js');
+const guessLeagueStatus = require('./guessLeagueStatus.js');
 
 module.exports = {
   errorUtils,
   passwordUtils,
   profileUtils,
   queryUtils,
-  statusUtils
+  statusUtils,
+  guessLeagueStatus
 }
---
+++ Utils/guessLeagueStatus.js
@@ -0,0 +1,8 @@
+const guessLeagueStatus = {
+  INVITED: 'invited',
+  PLAYING: 'playing',
+  DECLINED: 'declined',
+  QUITTED: 'quitted'
+}
+
+module.exports = Object.freeze(guessLeagueStatus);
