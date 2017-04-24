893312b3cb18595de7ccf3bc6af28a0366eeb348
Lucas Alves <lucashalves@live.com>
2017-04-24 14:52:52 -0300

    Update ptBR.js

--- Translate/ptBR.js
+++ Translate/ptBR.js
@@ -1,9 +1,9 @@
 'use Strict';
 
-const usEnDictionary = {
+const ptBrDictionary = {
   passwordAlert: 'A Senha deve conter ao menos 8 caracteres, 1 caracter especial, 1 Letra Maiúscula, 1 número e 1 letra Minúscula',
   alreadyUsed: 'Email ou NickName já está em uso',
   invalidLogin: 'Usuário ou Senha incorreta, tente novamente'
 }
 
-module.exports = usEnDictionary
+module.exports = ptBrDictionary
