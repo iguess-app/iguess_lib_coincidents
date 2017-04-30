7e8cbc2b40d697a278ce469b30811ca762574797
Lucas Alves <lucashalves@live.com>
2017-04-30 17:29:48 -0300

    Connecting Mongo from MongoAtlas address.

--- Managers/mongoManager.js
+++ Managers/mongoManager.js
@@ -6,15 +6,15 @@ const promise = require('bluebird');
 const config = require('./../config');
 const mongo = config.mongo;
 
-const uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`;
-//const uri = `mongodb://${mongo.user}:${mongo.password}@${mongo.address}/${mongo.database}?ssl=true&replicaSet=ZeroCluster-shard-0&authSource=admin`;
+//const uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`;
+const uri = `mongodb://${mongo.user}:${mongo.password}@${mongo.address}/${mongo.database}?ssl=true&replicaSet=ZeroCluster-shard-0&authSource=admin`;
 const options = {
   promiseLibrary: promise
 };
 const db = mongoose.createConnection(uri, options);
 
 db.on('open', () => {
-  console.info(`Mongo at ${db.host} (database:${db.name}) Connected`)
+  console.info(`Mongo (database:${db.name}) Connected`)
 });
 
 module.exports = db;
