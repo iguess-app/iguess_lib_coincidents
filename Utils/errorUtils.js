cda7332de4520188756e4c5d9ad380763e91a204
Lucas Alves <lucashalves@live.com>
2017-05-04 22:59:57 -0300

    Update errorUtil enum

--- Utils/errorUtils.js
+++ Utils/errorUtils.js
@@ -1,14 +1,13 @@
 'use Strict';
 
-const mongoErrors = {
+const mongoErrors = Object.freeze({
   _idAlreadyUsed: 11000
-}
+})
 
-const userErrors = {
-  passwordInvalid: {
-    code: 20000
-  }
-}
+const userErrors = Object.freeze({
+  passwordInvalid: 20000,
+  notEmail: 20001
+})
 
 module.exports = {
   mongoErrors,
