8e1d29127611592ae12c81dd5dad5b3e07633698
Lucas Alves <lucashalves@live.com>
2017-05-04 22:57:50 -0300

    Added isEmail at profileUtil

--- Utils/profileUtils.js
+++ Utils/profileUtils.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const errorUtils = require('./errorUtils.js');
+const userErrors = errorUtils.userErrors;
+
 const useNicknameLikeID = (user) => {
   Reflect.set(user, '_id', user.nickName)
   Reflect.deleteProperty(user, 'nickName');
@@ -14,7 +17,17 @@ const useIDLikeNickname = (user) => {
   return user;
 }
 
+const isEmail = (email) => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regex.test(email)) {
+    return true
+  }
+  return userErrors.notEmail;
+}
+
 module.exports = {
   useNicknameLikeID,
-  useIDLikeNickname
+  useIDLikeNickname,
+  isEmail
 }
