f7b56ca716cdd9b7579174c2d8657d604eb83d93
Lucas Alves <lucashalves@live.com>
2017-05-04 21:34:40 -0300

    Getting every variables from process.env.

--- config.js
+++ config.js
@@ -1,33 +1,27 @@
-//TO DO
-//PUT EVERYTHING ON ENV VARIABLES
-
 module.exports = {
+  env: process.env.ENVIRONMENT || 'development',
   token: {
-    cert: 'asdawUGDQ&@ET*@&GUGDASU89yHdausdg231',
-    expirationTime: 3600 //1hour (in Seconds)
+    cert: process.env.TOKEN_SEED || 'asdawUGDQ&@ET*@&GUGDASU89yHdausdg231',
+    expirationTime: process.env.TOKEN_EXPIRATION_TIME || 3600 //1hour (in Seconds)
   },
-  salt: '$2a$10$5PMJupkGGUJ22DxQC4UoUe',
+  salt: process.env.SALT || '$2a$10$5PMJupkGGUJ22DxQC4UoUe',
+  redis: {
+    host: process.env.REDIS_HOST || '130.211.171.212',
+    port: process.env.REDIS_PORT || 6379,
+    key: process.env.REDIS_KEY || 'wAMK4Hig',
+    defaultExpireTime: process.env.REDIS_EXPIRE_TIME || 60 //1Minute (in Seconds)
+  },
+  mongo: {
+    user: process.env.MONGO_USER || 'luhalvesbr',
+    password: process.env.MONGO_KEY || 'xSxotVAFrlegS2yJ',
+    address: process.env.MONGO_ADDRESS || 'zerocluster-shard-00-00-mfooh.mongodb.net:27017,zerocluster-shard-00-01-mfooh.mongodb.net:27017,zerocluster-shard-00-02-mfooh.mongodb.net:27017',
+    database: process.env.MONGO_DB || 'iguess',
+    atlas: process.env.MONGO_ATLAS || false,
+    port: process.env.MONGO_PORT || 27017,
+    host: process.env.MONGO_HOST || 'localhost',
+  },
   facebook: {
     clientSecret: '4bea584f60c0cf37b5b037b1c4bc8590',
     clientID: '1839068873039445'
-  },
-  redis: {
-    host: '130.211.171.212',
-    port: 6379,
-    key: 'wAMK4Hig',
-    defaultExpireTime: 60 //1Minute (in Seconds)
-  },
-  mongo: {
-    user: 'luhalvesbr',
-    password: 'xSxotVAFrlegS2yJ',
-    address: 'zerocluster-shard-00-00-mfooh.mongodb.net:27017,zerocluster-shard-00-01-mfooh.mongodb.net:27017,zerocluster-shard-00-02-mfooh.mongodb.net:27017',
-    port: null,
-    database: 'iguess'
-  },
-  // mongo: {
-  //   host: 'localhost',
-  //   port: '27017',
-  //   database: 'iguess'
-  // },
-  routes: []
+  }
 }
