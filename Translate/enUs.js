a60424f1906b47dfa01f824ebd9c9caf279ff443
Lucas Alves <lucashalves@live.com>
2017-05-04 23:08:46 -0300

    Added at dictionary the term 'notAEmail'

--- Translate/enUs.js
+++ Translate/enUs.js
@@ -3,7 +3,8 @@
 const usEnDictionary = {
   passwordAlert: 'Password must be at least 8 characters, 1 special char, 1 Capital Letter, 1 Number and 1 lowerCase',
   alreadyUsed: 'Email or NickName already used',
-  invalidLogin: 'User or Password incorrect, try again'
+  invalidLogin: 'User or Password incorrect, try again',
+  notAEmail: 'Not is a valid email'
 }
 
 module.exports = usEnDictionary
--- Translate/ptBR.js
+++ Translate/ptBR.js
@@ -3,7 +3,8 @@
 const ptBrDictionary = {
   passwordAlert: 'A Senha deve conter ao menos 8 caracteres, 1 caracter especial, 1 Letra Maiúscula, 1 número e 1 letra Minúscula',
   alreadyUsed: 'Email ou NickName já está em uso',
-  invalidLogin: 'Usuário ou Senha incorreta, tente novamente'
+  invalidLogin: 'Usuário ou Senha incorreta, tente novamente',
+  notAEmail: 'Não recebemos um email'
 }
 
 module.exports = ptBrDictionary
