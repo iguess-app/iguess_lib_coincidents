15c19a889784d9141c2da472057101042b03ab3c
Lucas Alves <lucashalves@live.com>
2017-05-06 02:04:14 -0300

    Updating dictionary

--- Translate/enUS.js
+++ Translate/enUS.js
@@ -4,7 +4,9 @@ const usEnDictionary = {
   passwordAlert: 'Password must be at least 8 characters, 1 special char, 1 Capital Letter, 1 Number and 1 lowerCase',
   alreadyUsed: 'Email or NickName already used',
   invalidLogin: 'User or Password incorrect, try again',
-  notAEmail: 'Not is a valid email'
+  notAEmail: 'Not is a valid email',
+  invalidPassword: 'Password incorrect, try again',
+  tooManyInvalidPassword: 'You did some many requests with wrong passwords, wait 1 hour to try it again'
 }
 
 module.exports = usEnDictionary
--- Translate/ptBR.js
+++ Translate/ptBR.js
@@ -4,7 +4,9 @@ const ptBrDictionary = {
   passwordAlert: 'A Senha deve conter ao menos 8 caracteres, 1 caracter especial, 1 Letra Maiúscula, 1 número e 1 letra Minúscula',
   alreadyUsed: 'Email ou NickName já está em uso',
   invalidLogin: 'Usuário ou Senha incorreta, tente novamente',
-  notAEmail: 'Não recebemos um email'
+  notAEmail: 'Não recebemos um email',
+  invalidPassword: 'Senha incorreta, tente novamente',
+  tooManyInvalidPassword: 'Você fez muitas requisições com senhas erradas, espere 1 hora para tentar novamente'
 }
 
 module.exports = ptBrDictionary
