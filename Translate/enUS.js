534b926fd0870bd990e45af291a9bc3470e87328
Lucas Alves <lucashalves@live.com>
2017-05-07 02:28:05 -0300

    Changing dictionaries msgs

--- Translate/enUS.js
+++ Translate/enUS.js
@@ -3,8 +3,8 @@
 const usEnDictionary = {
   passwordAlert: 'Password must be at least 8 characters, 1 special char, 1 Capital Letter, 1 Number and 1 lowerCase',
   alreadyUsed: 'Email or UserName already used',
-  userNameAlreadyUsed: 'This userName is already used by other user',
-  emailAlreadyUsed: 'This email is already used by other user',
+  userNameAlreadyUsed: 'This userName is already used by other account',
+  emailAlreadyUsed: 'This email is already used by other account',
   invalidLogin: 'User or Password incorrect, try again',
   notAEmail: 'Not is a valid email',
   invalidPassword: 'Password incorrect, try again',
--- Translate/ptBR.js
+++ Translate/ptBR.js
@@ -3,8 +3,8 @@
 const ptBrDictionary = {
   passwordAlert: 'A Senha deve conter ao menos 8 caracteres, 1 caracter especial, 1 Letra Maiúscula, 1 número e 1 letra Minúscula',
   alreadyUsed: 'Email ou UserName já está em uso',
-  userNameAlreadyUsed: 'Esse userName já está em uso por outro usuário',
-  emailAlreadyUsed: 'Esse Email já está em uso por outro usuário',
+  userNameAlreadyUsed: 'Esse userName já está em uso por outra conta',
+  emailAlreadyUsed: 'Esse Email já está em uso por outra conta',
   invalidLogin: 'Usuário ou Senha incorreta, tente novamente',
   notAEmail: 'Não recebemos um email válido',
   invalidPassword: 'Senha incorreta, tente novamente',
