3c9cfcb9468fd0a8f1dcecaa1bd159675ece247e
Lucas Alves <lucashalves@live.com>
2017-05-07 00:35:10 -0300

    Updating dictionaries

--- Translate/enUS.js
+++ Translate/enUS.js
@@ -2,7 +2,9 @@
 
 const usEnDictionary = {
   passwordAlert: 'Password must be at least 8 characters, 1 special char, 1 Capital Letter, 1 Number and 1 lowerCase',
-  alreadyUsed: 'Email or NickName already used',
+  alreadyUsed: 'Email or UserName already used',
+  userNameAlreadyUsed: 'This userName is already used by other user',
+  emailAlreadyUsed: 'This email is already used by other user',
   invalidLogin: 'User or Password incorrect, try again',
   notAEmail: 'Not is a valid email',
   invalidPassword: 'Password incorrect, try again',
--- Translate/ptBR.js
+++ Translate/ptBR.js
@@ -2,9 +2,11 @@
 
 const ptBrDictionary = {
   passwordAlert: 'A Senha deve conter ao menos 8 caracteres, 1 caracter especial, 1 Letra Maiúscula, 1 número e 1 letra Minúscula',
-  alreadyUsed: 'Email ou NickName já está em uso',
+  alreadyUsed: 'Email ou UserName já está em uso',
+  userNameAlreadyUsed: 'Esse userName já está em uso por outro usuário',
+  emailAlreadyUsed: 'Esse Email já está em uso por outro usuário',
   invalidLogin: 'Usuário ou Senha incorreta, tente novamente',
-  notAEmail: 'Não recebemos um email',
+  notAEmail: 'Não recebemos um email válido',
   invalidPassword: 'Senha incorreta, tente novamente',
   tooManyInvalidPassword: 'Você fez muitas requisições com senhas erradas, espere 1 hora para tentar novamente'
 }
