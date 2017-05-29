848094d2b1bb582ebb5a8226796895ccc34c0a8b
Lucas Alves <lucashalves@live.com>
2017-05-28 23:57:27 -0300

    Creating a rule to connect on mongo and redis according to a envVariable

--- Managers/mongoManager.js
+++ Managers/mongoManager.js
@@ -6,19 +6,21 @@ const promise = require('bluebird');
 const config = require('./../config');
 const mongo = config.mongo;
 
-let uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`;
+if (mongo.needConnection) {
+  let uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`;
 
-if (mongo.atlas) {
-  uri = `mongodb://${mongo.user}:${mongo.password}@${mongo.address}/${mongo.database}?ssl=true&replicaSet=ZeroCluster-shard-0&authSource=admin`;
+  if (mongo.atlas) {
+    uri = `mongodb://${mongo.user}:${mongo.password}@${mongo.address}/${mongo.database}?ssl=true&replicaSet=ZeroCluster-shard-0&authSource=admin`;
+  }
+
+  const options = {
+    promiseLibrary: promise
+  };
+  const db = mongoose.createConnection(uri, options);
+
+  db.on('open', () => {
+    console.info(`Mongo at ${uri} Connected`)
+  });
+
+  module.exports = db;
 }
-
-const options = {
-  promiseLibrary: promise
-};
-const db = mongoose.createConnection(uri, options);
-
-db.on('open', () => {
-  console.info(`Mongo at ${uri} Connected`)
-});
-
-module.exports = db;
--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 const Managers = require('./Managers/export');
 const Translate = require('./Translate/export');
 const Utils = require('./Utils/export');
-const Schemas = require('./Schemas/export');
 const Config = require('./config')
+let Schemas;
+if(Config.mongo.needConnection){
+  Schemas = require('./Schemas/export');
+}
 
 module.exports = {
   Managers,
