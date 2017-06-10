55e318f09c18707e5ea0dcb70bd0f755f93da128
Lucas Alves <lucashalves@live.com>
2017-06-10 19:11:46 -0300

    Updating configJs

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 module.exports = {
   env: process.env.ENV || 'local',
   token: {
-    cert: process.env.TOKEN_SEED || 'asdawUGDQ&@ET*@&GUGDASU89yHdausdg231',
+    cert: process.env.TOKEN_SEED || 'asdawUGDQ&@ET*@&GUGDASU89yHdausdg231', //Open just before send the project to Prod
     expirationTime: parseInt(process.env.TOKEN_EXPIRATION_TIME) || 3600 //1hour (in Seconds)
   },
-  salt: process.env.SALT || '$2a$10$5PMJupkGGUJ22DxQC4UoUe',
+  salt: process.env.SALT || '$2a$10$5PMJupkGGUJ22DxQC4UoUe', //Open just before send the project to Prod
   redis: {
     needConnection: parseInt(process.env.REDIS_CONNECTION) || 1,
-    host: process.env.REDIS_HOST || '130.211.171.212',
+    host: process.env.REDIS_HOST || '',
     port: process.env.REDIS_PORT || 6379,
-    key: process.env.REDIS_KEY || 'wAMK4Hig',
+    key: process.env.REDIS_KEY || '',
     defaultExpireTime: parseInt(process.env.REDIS_EXPIRE_TIME) || 60 //1Minute (in Seconds)
   },
   mongo: {
     needConnection: parseInt(process.env.MONGO_CONNECTION) || 1,
-    user: process.env.MONGO_USER || 'luhalvesbr',
-    password: process.env.MONGO_KEY || 'xSxotVAFrlegS2yJ',
-    address: process.env.MONGO_ADDRESS || 'zerocluster-shard-00-00-mfooh.mongodb.net:27017,zerocluster-shard-00-01-mfooh.mongodb.net:27017,zerocluster-shard-00-02-mfooh.mongodb.net:27017',
+    user: process.env.MONGO_USER || '',
+    password: process.env.MONGO_KEY || '',
+    address: process.env.MONGO_ADDRESS || '',
     database: process.env.MONGO_DB || 'iguess',
     atlas: process.env.MONGO_ATLAS || false,
     port: process.env.MONGO_PORT || 27017,
@@ -27,10 +27,6 @@ module.exports = {
     holiUrl: process.env.HOLI_URL || 'http://127.0.0.1:9001',
     personalUrl: process.env.PERSONAL_URL || 'http://127.0.0.1:9002',
     guessUrl: process.env.GUESS_URL || 'http://127.0.0.1:9003'
-  },
-  facebook: {
-    clientSecret: '4bea584f60c0cf37b5b037b1c4bc8590',
-    clientID: '1839068873039445'
   },
   profile: {
     maxTeamToAppreciateAllowed: 2,
