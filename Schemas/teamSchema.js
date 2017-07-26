e0cb801dcd629cc17f00bc3dbadb30c4f889f9c5
Lucas Alves <lucashalves@live.com>
2017-07-25 22:11:58 -0300

    Added ID ref size validate at TeamSchema

--- Schemas/teamSchema.js
+++ Schemas/teamSchema.js
@@ -1,19 +1,28 @@
 'use strict'
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
+const Config = require('./../config')
 const Managers = require('./../Managers/export');
+
+const Schema = mongoose.Schema;
 const db = Managers.mongoManager;
 
 const optionsSchema = {
   versionKey: false
 }
 
+/*
+  TODO:
+  Colocar NotAIdSize num padrao de erro do projeto
+*/
+const checkIdSize = (id) => id.length === Config.mongo.idStringSize
+
 const teamSchema = new Schema({
   league: {
     type: String,
-    required: true
+    required: true,
+    validate: [checkIdSize, 'NotAIdSize']
   },
   fullName: {
     type: String,
