104289124854f70ccca1d13d420e4e0083cd0671
Lucas Alves <lucashalves@live.com>
2017-08-23 23:23:12 -0300

    Allow String to RoundRobin Tournaments on fixtureValidator

--- Schemas/subValidations/fixture.js
+++ Schemas/subValidations/fixture.js
@@ -5,6 +5,6 @@ const KNOCKOUT_TOURNAMENT_ROUND_NAMES = Config.holi.knockoutTournamentRoundNames
 
 const _ifIsKnockoutTournament = (fixture) => KNOCKOUT_TOURNAMENT_ROUND_NAMES.includes(fixture)
 const _ifIsRoundRobinTournament = (fixture) => fixture >= MIN_ROUND_ROBIN_FIXTURES && fixture <= MAX_ROUND_ROBIN_FIXTURES && Number.isInteger(fixture)
-const validateFixture = (fixture) => _ifIsKnockoutTournament(fixture) || _ifIsRoundRobinTournament(fixture)
+const validateFixture = (fixture) => _ifIsKnockoutTournament(fixture) || _ifIsRoundRobinTournament(Number(fixture))
 
 module.exports = validateFixture
