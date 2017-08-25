11c3f530c8c93d77b38f14556772fe38ef026a90
Lucas Alves <lucashalves@live.com>
2017-08-24 21:44:52 -0300

    Adapting guessline Schema to fit the scenario ate setPrediction

--- Schemas/guessesLinesSchema.js
+++ Schemas/guessesLinesSchema.js
@@ -4,12 +4,15 @@ const mongoose = require('mongoose')
 
 const Managers = require('../Managers/export')
 const Utils = require('../Utils/export')
+const validateFixture = require('./subValidations/fixture')
+const championshipFixtureUserKeyValidator = require('./subValidations/championshipFixtureUserKey')
+const mongo = require('../config').mongo
 
 const Schema = mongoose.Schema
+const Mixed = Schema.Types.Mixed
 const db = Managers.mongoManager
-
-const mongo = require('../config').mongo
 const serverErrors = Utils.errorUtils.serverErrors
+const userErrors = Utils.errorUtils.userErrors
 
 const optionsSchema = {
   versionKey: false
@@ -41,9 +44,19 @@ const championshipSchema = new Schema({
   }
 }, optionsSchemaNoIdNoVersion)
 
+
 const fixturesSchema = {
-  type: Object,
-  required: true
+  fixture: {
+    type: Mixed,
+    required: true,
+    validate: [validateFixture, String(userErrors.notValidFixture)]
+  },
+  usersWhoAlreadySentGuesses: {
+    type: [{
+      type: String,
+      validate: [championshipFixtureUserKeyValidator, String(serverErrors.notchampionshipFixtureUserKeyValid)]
+    }]
+  }
 }
 
 const guessesLinesSchema = new Schema({
@@ -51,9 +64,15 @@ const guessesLinesSchema = new Schema({
     type: championshipSchema,
     required: true
   },
-  fixtures: fixturesSchema,
-  users: {
-    type: [String]
+  fixtures: {
+    type: [fixturesSchema],
+    required: true
+  },
+  usersAddedAtGuessLine: {
+    type: [{
+      type: String,
+      validate: [mongo.checkObjectId, String(serverErrors.notMongoIdValid)]
+    }]
   }
 }, optionsSchema)
 
