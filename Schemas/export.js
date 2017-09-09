14dd9ee7aeadb2d03092b6747f39e4f710154622
Lucas Alves <lucashalves@live.com>
2017-09-09 15:04:24 -0300

    exportSchema file updates

--- Schemas/export.js
+++ Schemas/export.js
@@ -1,15 +1,7 @@
-const championshipSchema = require('./championshipSchema')
-const leagueSchema = require('./leagueSchema')
 const profileSchema = require('./profileSchema')
-const roundSchema = require('./roundSchema')
-const teamSchema = require('./teamSchema')
 const notificationsSchema = require('./notificationsSchema')
 
 module.exports = {
-  championshipSchema,
-  leagueSchema,
   profileSchema,
-  roundSchema,
-  teamSchema,
   notificationsSchema
 }
