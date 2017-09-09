478af94427e2fc72c2f9dc9f899063f9c8d6abbf
Lucas Alves <lucashalves@live.com>
2017-09-09 14:15:05 -0300

    updating export schema

--- Schemas/export.js
+++ Schemas/export.js
@@ -1,23 +1,15 @@
 const championshipSchema = require('./championshipSchema')
-const guessesLeaguesSchema = require('./guessesLeaguesSchema')
-const guessesLinesSchema = require('./guessesLinesSchema')
 const leagueSchema = require('./leagueSchema')
 const profileSchema = require('./profileSchema')
 const roundSchema = require('./roundSchema')
 const teamSchema = require('./teamSchema')
 const notificationsSchema = require('./notificationsSchema')
-const predictionsSchema = require('./predictionsSchema')
-const pontuationsSchema = require('./pontuationsSchema')
 
 module.exports = {
   championshipSchema,
-  guessesLeaguesSchema,
-  guessesLinesSchema,
   leagueSchema,
   profileSchema,
   roundSchema,
   teamSchema,
-  notificationsSchema,
-  predictionsSchema,
-  pontuationsSchema
+  notificationsSchema
 }
