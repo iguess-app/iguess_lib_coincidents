1db64b312216bb2d201c3cbac8dc014e8318c74e
Lucas Alves <lucashalves@live.com>
2017-09-23 18:55:10 -0300

    Added to requestManager a option to build QueryString

--- Managers/requestManager.js
+++ Managers/requestManager.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const requestPromise = require('request-promise');
+const requestPromise = require('request-promise')
+const qs = require('querystring')
 
 const requestManager = {
   post: (uri, reqHeaders, body) => {
@@ -37,10 +38,30 @@ const requestManager = {
     return requestPromise(options)
   },
 
-  get: (uri, reqHeaders) => {
+  patch: (uri, reqHeaders, body) => {
     const headers = {};
     if (reqHeaders) {
       headers.language = reqHeaders.language
+    }
+
+    const options = {
+      method: 'PATCH',
+      uri,
+      headers,
+      body,
+      json: true
+    };
+
+    return requestPromise(options)
+  },
+
+  get: (uri, reqHeaders, querystring) => {
+    const headers = {}
+    if (reqHeaders) {
+      headers.language = reqHeaders.language
+    }
+    if (querystring) {
+      _buildQueryString(querystring)
     }
 
     const options = {
@@ -48,11 +69,32 @@ const requestManager = {
       uri,
       headers,
       json: true
-    };
+    }
 
     return requestPromise(options)
   }
 
 }
+
+const _buildQueryString = (obj) => {
+  const keys = Object.keys(obj)
+  const values = Object.values(obj)
+  
+  const qsBuilded = keys.reduce((acumulator, key, index) => {
+    const qsObj = {}
+    qsObj[key] = values[index]
+    const partialQS = qs.stringify(qsObj)
+    
+    if (acumulator.length) {
+      return `${acumulator}&${partialQS}` 
+    }
+    
+    return acumulator + partialQS
+  }, '?')
+  
+  
+  return qsBuilded
+}
+
 
 module.exports = requestManager
