e23eaa5a67b9809eb9b1ce1cff6b1eaece24adf8
Lucas Alves <lucashalves@live.com>
2017-10-04 00:10:03 -0300

    Let multiples connections at mongo

--- Managers/mongoManager.js
+++ Managers/mongoManager.js
@@ -7,12 +7,7 @@ const pino = require('./logManager')
 const config = require('../config/config')
 const mongo = config.mongo
 
-let DB_GLOBAL_CONNECTION = null
-
 const connect = (uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`) => {
-  if (DB_GLOBAL_CONNECTION) {
-    return DB_GLOBAL_CONNECTION
-  }
 
   if (mongo.atlas) {
     uri = `mongodb://${mongo.user}:${mongo.password}@${mongo.address}/${mongo.database}?ssl=true&replicaSet=ZeroCluster-shard-0&authSource=admin`
@@ -22,15 +17,15 @@ const connect = (uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`
     promiseLibrary: promise
   }
 
-  DB_GLOBAL_CONNECTION = mongoose.createConnection(uri, options)
+  const db = mongoose.createConnection(uri, options)
 
-  DB_GLOBAL_CONNECTION.on('open', () => {
+  db.on('open', () => {
     pino.info(`Mongo at ${uri} Connected`)
   })
 
   mongoose.set('debug', true)
 
-  return DB_GLOBAL_CONNECTION
+  return db
 }
 
 module.exports = connect
