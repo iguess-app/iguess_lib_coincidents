1f1fe44269c427bce7432f4f61398a900f01b10e
Lucas Alves <lucashalves@live.com>
2017-10-04 00:08:30 -0300

    Mongo treatment to use only one connection

--- Managers/mongoManager.js
+++ Managers/mongoManager.js
@@ -1,35 +1,36 @@
 'use strict'
 
-const mongoose = require('mongoose');
-const promise = require('bluebird');
+const mongoose = require('mongoose')
+const promise = require('bluebird')
 const pino = require('./logManager')
 
-const config = require('../config/config');
-const mongo = config.mongo;
+const config = require('../config/config')
+const mongo = config.mongo
+
+let DB_GLOBAL_CONNECTION = null
 
 const connect = (uri = `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`) => {
-
-  if (mongo.needConnection) {
-
-    if (mongo.atlas) {
-      uri = `mongodb://${mongo.user}:${mongo.password}@${mongo.address}/${mongo.database}?ssl=true&replicaSet=ZeroCluster-shard-0&authSource=admin`;
-    }
-
-    const options = {
-      promiseLibrary: promise
-    };
-    const db = mongoose.createConnection(uri, options);
-
-    db.on('open', () => {
-      pino.info(`Mongo at ${uri} Connected`)
-    });
-
-    mongoose.set('debug', true);
-
-    return db
+  if (DB_GLOBAL_CONNECTION) {
+    return DB_GLOBAL_CONNECTION
   }
 
-  return false
+  if (mongo.atlas) {
+    uri = `mongodb://${mongo.user}:${mongo.password}@${mongo.address}/${mongo.database}?ssl=true&replicaSet=ZeroCluster-shard-0&authSource=admin`
+  }
+
+  const options = {
+    promiseLibrary: promise
+  }
+
+  DB_GLOBAL_CONNECTION = mongoose.createConnection(uri, options)
+
+  DB_GLOBAL_CONNECTION.on('open', () => {
+    pino.info(`Mongo at ${uri} Connected`)
+  })
+
+  mongoose.set('debug', true)
+
+  return DB_GLOBAL_CONNECTION
 }
 
 module.exports = connect
