8b94e30bc5f7bf71390879ee67e153e83b8be3e7
Lucas Alves <lucashalves@live.com>
2017-10-18 00:00:11 -0200

    Refactorting at logManager

--- Managers/logManager.js
+++ Managers/logManager.js
@@ -1,10 +1,41 @@
+'use strict'
+
 const pino = require('pino')
+
+const env = require('../config/config').env
+let log = {}
 
 const pretty = pino.pretty()
 pretty.pipe(process.stdout)
 
-const log = pino({
-  safe: true
-}, pretty)
+const isLoggableEnv = () => env === 'dev' || env === 'homolog' || env === 'beta'
 
-module.exports = log
+if (isLoggableEnv()) {
+  log = pino({
+    safe: true
+  }, pretty)
+}
+
+module.exports = {
+  info: (message) => {
+    if (isLoggableEnv()) {
+      log.info(message)
+    }
+  },
+  error: (message) => {
+    if (isLoggableEnv()) {
+      log.error(message)
+    }
+  },
+  warn: (message) => {
+    if (isLoggableEnv()) {
+      log.warn(message)
+    }
+  },
+  isLoggableEnv
+}
+
+/**
+ * @function isLoggableEnv
+ * @return {boolean}
+*/
