219c045d621bf6ea3ca9b51caa1a8f6affb1753f
Lucas Alves <lucashalves@live.com>
2017-11-15 17:59:29 -0200

    Profile TreatError update

--- Utils/profileUtils.js
+++ Utils/profileUtils.js
@@ -18,18 +18,18 @@ const treatErrors = (err, dictionary) => {
   switch (err.code) {
     case Errors.mongoErrors._idAlreadyUsed:
       if (err.message.includes('userName')) {
-        throw Boom.notAcceptable(`${dictionary.userNameAlreadyUsed}.`);
+        throw Boom.notAcceptable(dictionary.userNameAlreadyUsed);
       }
       if (err.message.includes('email')) {
-        throw Boom.notAcceptable(`${dictionary.emailAlreadyUsed}.`);
+        throw Boom.notAcceptable(dictionary.emailAlreadyUsed);
       }
       throw Boom.badData(err.message)
     case userErrors.userNameSizeExplode:
-      throw Boom.notAcceptable(`${dictionary.tooLongUserName}.`);
+      throw Boom.notAcceptable(dictionary.tooLongUserName);
     case userErrors.nameSizeExplode:
-      throw Boom.notAcceptable(`${dictionary.tooLongName}.`);
+      throw Boom.notAcceptable(dictionary.tooLongName);
     case userErrors.descriptionSizeExplode:
-      throw Boom.notAcceptable(`${dictionary.tooLongDescription}.`);
+      throw Boom.notAcceptable(dictionary.tooLongDescription);
     default:
       throw Boom.badData(err.message)
   }
