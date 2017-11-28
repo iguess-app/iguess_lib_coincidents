7e62875c16aa5f1179437a7bcb65b6787b41f809
Lucas Alves <lucashalves@live.com>
2017-11-28 18:29:58 -0200

    Update dictionary

--- Translate/enUS.js
+++ Translate/enUS.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const usEnDictionary = {
+  sessionExpired: 'Your session expired. Please sign in again',
   passwordAlert: 'Password must be at least 8 characters, 1 special char, 1 Capital Letter, 1 Number and 1 lowerCase',
   alreadyUsed: 'Email or UserName already used',
   userNameAlreadyUsed: 'This userName is already used by other account',
--- Translate/ptBR.js
+++ Translate/ptBR.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const ptBrDictionary = {
+  sessionExpired: 'Sua sessão expirou, faça login novamente',
   passwordAlert: 'A Senha deve conter ao menos 8 caracteres, 1 caracter especial, 1 Letra Maiúscula, 1 número e 1 letra Minúscula',
   alreadyUsed: 'Email ou UserName já está em uso',
   userNameAlreadyUsed: 'Esse userName já está em uso por outra conta',
