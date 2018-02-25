c10afd03d1da8a9d233d889f29eb4591e039f470
Lucas Alves <lucashalves@live.com>
2018-02-25 12:44:04 -0300

    cacheManager bug fixes

--- Managers/cacheManager.js
+++ Managers/cacheManager.js
@@ -7,98 +7,94 @@ const pino = require('./logManager')
 const config = require('../config/config');
 
 const THREE_SECONDS = 3000
+let CacheManager = {}
 
-const clientOptions = {
-  'auth_pass': config.redis.key,
-  'retry_strategy': (options) => {
-    if (options.error) {
-      pino.error(options.error.message);
-    }
-
-    return THREE_SECONDS
-  }
-};
-
-const redisClient = redis.createClient(config.redis.port, config.redis.host, clientOptions);
-
-const connect = () => {
-  if (config.redis.needConnection) {
-    redisClient.on('connect', () => {
-      if (redisClient.connected === true) {
-        pino.info(`Redis at ${redisClient.address} Connected`)
-      }
-    })
-  }
-}
-connect()
-
-redisClient.on('error', (err) => {
-  pino.error(err);
-  connect()
-
-  return true;
-});
-
-const isConnected = () => {
-  if (!redisClient.connected) {
-    pino.error(`Redis ${redisClient.address} not connected`)
-    connect()
-    
-    return false
-  }
-
-  return true
-}
-
-const CacheManager = {
-  get: (key) => {
-    if (!key) {
-      throw new Error('key is a mandatory field to get on Cache.')
-    }
-    const md5Key = _generateKey(key);
-
-    return new Promise((resolve) => {
-      if (!isConnected(redisClient)) {
-        return resolve(null)
+if (config.redis.needConnection) {
+  const clientOptions = {
+    'auth_pass': config.redis.key,
+    'retry_strategy': (options) => {
+      if (options.error) {
+        pino.error(options.error.message);
       }
 
-      return redisClient.get(md5Key, (err, value) => {
-        if (err) {
-          pino.error(err)
+      return THREE_SECONDS
+    }
+  };
 
+  const redisClient = redis.createClient(config.redis.port, config.redis.host, clientOptions);
+
+  redisClient.on('connect', () => {
+    if (redisClient.connected === true) {
+      pino.info(`Redis at ${redisClient.address} Connected`)
+    }
+  })
+
+  redisClient.on('error', (err) => {
+    pino.error(err);
+
+    return true;
+  });
+
+  const isConnected = () => {
+    if (!redisClient.connected) {
+      pino.error(`Redis ${redisClient.address} not connected`)
+
+      return false
+    }
+
+    return true
+  }
+
+  CacheManager = {
+    get: (key) => {
+      if (!key) {
+        throw new Error('key is a mandatory field to get on Cache.')
+      }
+      const md5Key = _generateKey(key);
+
+      return new Promise((resolve) => {
+        if (!isConnected(redisClient)) {
           return resolve(null)
         }
-        resolve(JSON.parse(value))
+
+        return redisClient.get(md5Key, (err, value) => {
+          if (err) {
+            pino.error(err)
+
+            return resolve(null)
+          }
+          resolve(JSON.parse(value))
+        })
       })
-    })
-  },
-  set: (key, value, expireTime = config.redis.defaultExpireTime) => {
-    if (!isConnected(redisClient)) {
-      return null
-    }
-    if (!key || !value) {
-      throw new Error('key and value are mandatory fields to set on Cache.')
-    }
-
-    const md5Key = _generateKey(key)
-
-    return redisClient.setex(md5Key, expireTime, JSON.stringify(value))
-  },  
-  del: (key) => {
-    if (!key) {
-      throw new Error('key is a mandatory field to get on Cache.')
-    }
-    const md5Key = _generateKey(key);
-
-    return new Promise((resolve) => {
+    },
+    set: (key, value, expireTime = config.redis.defaultExpireTime) => {
       if (!isConnected(redisClient)) {
-        return resolve(null)
+        return null
+      }
+      if (!key || !value) {
+        throw new Error('key and value are mandatory fields to set on Cache.')
       }
 
-      resolve(redisClient.del(md5Key))
-    })
+      const md5Key = _generateKey(key)
+
+      return redisClient.setex(md5Key, expireTime, JSON.stringify(value))
+    },
+    del: (key) => {
+      if (!key) {
+        throw new Error('key is a mandatory field to get on Cache.')
+      }
+      const md5Key = _generateKey(key);
+
+      return new Promise((resolve) => {
+        if (!isConnected(redisClient)) {
+          return resolve(null)
+        }
+
+        resolve(redisClient.del(md5Key))
+      })
+    }
   }
-};
+}
 
 const _generateKey = (key) => md5(key)
 
