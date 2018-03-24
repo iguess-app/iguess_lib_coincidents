41b0ab695cc5997b91bed1d340b417fd8db2720b
Lucas Alves <lucashalves@live.com>
2018-03-24 16:01:20 -0300

    Change variable function, now it makes more sense

--- Managers/dateManager.js
+++ Managers/dateManager.js
@@ -5,7 +5,15 @@ const moment = require('moment-timezone')
 const config = require('../config/config')
 
 const UTC_ALIAS = 'UTC'
+const DAY_ALIAS = 'day'
+const FORMAT_TO_FORCE_DATE_WITHOUT_HOUR = 'YYYY-MM-DDZ'
 const API_FOOTBALL_TIME_ZONE = config.apiFootball.timezone
+
+const dateDictionary = {
+  YESTERDAY: -1,
+  TODAY: 0,
+  TOMMORROW: 1
+}
 
 const getDate = (date, dateFormat = '', dateOutput = '', timeZone = UTC_ALIAS, language = 'en-us') => {
   moment.locale(language)
@@ -21,10 +29,22 @@ const convertAPIFootballToUTC = (date, dateFormat = '', dateOutput = '') => {
 
 const getUTCNow = (dateOutput = '') => moment().utc().format(dateOutput)
 
+const getISODateWithMidNight = (chosenDay) => {
+  const dateWithoutHour = moment.tz(UTC_ALIAS).format(FORMAT_TO_FORCE_DATE_WITHOUT_HOUR)
+  const isoDateWihiMidNight = moment(dateWithoutHour, FORMAT_TO_FORCE_DATE_WITHOUT_HOUR)
+    .add(dateDictionary[chosenDay], DAY_ALIAS)
+    .utc()
+    .format()
+
+  return isoDateWihiMidNight
+}
+
 module.exports = {
   getDate,
   getUTCNow,
-  convertAPIFootballToUTC
+  convertAPIFootballToUTC,
+  getISODateWithMidNight,
+  dateDictionary
 }
 
 /*eslint max-params: 0 */
