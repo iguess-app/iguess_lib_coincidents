63d51ff54ae2d70a0c9d8704ed91b7a9a4aa286f
Lucas Alves <lucashalves@live.com>
2018-04-30 18:56:18 -0300

    Added errorCode to profile errors

--- Utils/profileUtils.js
+++ Utils/profileUtils.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const Boom = require('boom')
+
 const Errors = require('./errorUtils.js')
+const errorCode = require('./errorCodeUtils')
+
 const userErrors = Errors.userErrors
+const boom = Errors.boom
 
 const isEmail = (email) => {
   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -18,18 +22,18 @@ const treatErrors = (err, dictionary) => {
   switch (err.code) {
     case Errors.mongoErrors._idAlreadyUsed:
       if (err.message.includes('userName')) {
-        throw Boom.notAcceptable(dictionary.userNameAlreadyUsed)
+        throw boom('notAcceptable', dictionary.userNameAlreadyUsed, errorCode.userNameAlreadyUsed)
       }
       if (err.message.includes('email')) {
-        throw Boom.notAcceptable(dictionary.emailAlreadyUsed)
+        throw boom('notAcceptable', dictionary.emailAlreadyUsed, errorCode.emailAlreadyUsed)
       }
-      throw Boom.badData(err.message)
+      throw Boom.badData(err.message)      
     case userErrors.userNameSizeExplode:
-      throw Boom.notAcceptable(dictionary.tooLongUserName)
+        throw boom('notAcceptable', dictionary.tooLongUserName, errorCode.tooLongUserName)
     case userErrors.nameSizeExplode:
-      throw Boom.notAcceptable(dictionary.tooLongName)
+        throw boom('notAcceptable', dictionary.tooLongName, errorCode.tooLongName)
     case userErrors.descriptionSizeExplode:
-      throw Boom.notAcceptable(dictionary.tooLongDescription)
+        throw boom('notAcceptable', dictionary.tooLongDescription, errorCode.tooLongDescription)
     default:
       throw Boom.badData(err.message)
   }
