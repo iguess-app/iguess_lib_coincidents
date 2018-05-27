a8cc4fb174e6e1b4cd3d914159f3d80b4c9eb052
Lucas Alves <lucashalves@live.com>
2018-05-27 14:52:21 -0300

    Exporting countries names

--- Translate/enUS/index.js
+++ Translate/enUS/index.js
@@ -1,6 +1,9 @@
+'use strict'
+
 const about = require('./about')
 const messages = require('./messages')
 const words = require('./words')
 const championships = require('./championships')
+const countries = require('./countries')
 
-module.exports = Object.assign(about, messages, words, championships)
+module.exports = Object.assign(about, messages, words, championships, countries)
