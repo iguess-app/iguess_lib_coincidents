c86d3b66e6d137d26b76091c435d22178c0dc439
Lucas Alves <lucashalves@live.com>
2018-06-09 21:17:32 -0300

    Email body and subjecct

---
+++ Translate/enUS/forgotMyPassEmail.js
@@ -0,0 +1,6 @@
+module.exports = {
+  forgotMyPassSubject: 'Hey mate, forgot your password body!?',
+  forgotMyPassBody: `Thats ok, we can help you! 
+    Your token is: {{token}}. Copy it and back to app, so you will can reset your password o/ <br><br>
+    If you did not ask to reset your password, don't worry. Just let the token expired by itself!`
+}
---
+++ Translate/ptBR/forgotMyPassEmail.js
@@ -0,0 +1,6 @@
+module.exports = {
+  forgotMyPassSubject: 'Parece que esqueceu seu senha né!?',
+  forgotMyPassBody: `Tudo bem, a gente te ajuda! 
+    Esse é seu token: {{token}}. Copia o token volta lá no app e reseta a senha o/ <br><br>
+    Se você não pediu para resetar a senha, fica tranquilo. É só deixar o token expirar!`
+}
