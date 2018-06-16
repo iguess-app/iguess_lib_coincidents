222b33fc16dd0338be8c2682923e7a11f7d547c7
Lucas Alves <lucashalves@live.com>
2018-06-16 19:15:07 -0300

    Improving the english at forgotMyPass

--- Translate/enUS/forgotMyPassEmail.js
+++ Translate/enUS/forgotMyPassEmail.js
@@ -1,11 +1,11 @@
 module.exports = {
-  forgotMyPassSubject: 'Hey mate, forgot your password body!?',
+  forgotMyPassSubject: 'Have you forgotten your password buddy!?',
   titleH1: ' Reset my password ',
-  descriptionPart1: ' You ask to redefine the password to @{{userName}}. Copy and past the follow number at forgot my password area ',
+  descriptionPart1: ' You ask to redefine the password to @{{userName}}. Copy and paste the following number at reset area ',
   descriptionHighLight: '“Already have the reset token”',
   descriptionPart2: '. If you did not ask for the reset, just ignore this e-mail.',
   yourTokenIs: ' Your token is: ',
-  afterToken: ' Copy token above and past at forgot my password session on ',
+  afterToken: ' Copy the token above and paste at forgot my password session on ',
   afterTokenHighLight: ' “Already have the reset token”. ',
   doubtsBottom: ' Still have any doubt? ',
   talkWithUsBottom: ' Text us at some social media '
