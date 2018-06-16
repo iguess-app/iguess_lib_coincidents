d8657406901086efb67e29723ecd4feb888f68c7
Lucas Alves <lucashalves@live.com>
2018-06-16 17:03:47 -0300

    forgot my pass in engish

--- Translate/enUS/forgotMyPassEmail.js
+++ Translate/enUS/forgotMyPassEmail.js
@@ -1,12 +1,12 @@
 module.exports = {
   forgotMyPassSubject: 'Hey mate, forgot your password body!?',
-  titleH1: ' Redefinir minha senha ',
-  descriptionPart1: ' Você solicitou uma redefinição de senha para @{{userName}}. Copie e cole o número abaixo na área de “esqueci minha senha” no link ',
-  descriptionHighLight: '“Já tenho o token para redefinição”',
-  descriptionPart2: '. Se você não fez essa solicitação, ignore este e-mail.',
-  yourTokenIs: ' Seu token é: ',
-  afterToken: ' Copie o código acima e cole na sessão de “esqueci minha senha” no link ',
-  afterTokenHighLight: ' “Já tenho o token para redefinição”. ',
-  doubtsBottom: ' Ficou com alguma dúvida? ',
-  talkWithUsBottom: ' Fale conosco nas redes sociais '
+  titleH1: ' Reset my password ',
+  descriptionPart1: ' You ask to redefine the password to @{{}}. Copy and past the follow number at forgot my password area ',
+  descriptionHighLight: '“Already have the reset token”',
+  descriptionPart2: '. If you did not ask for the reset, just ignore this e-mail.',
+  yourTokenIs: ' Your token is: ',
+  afterToken: ' Copy token above and past at forgot my password session on ',
+  afterTokenHighLight: ' “Already have the reset token”. ',
+  doubtsBottom: ' Still have any doubt? ',
+  talkWithUsBottom: ' Text us at some social media '
 }
