f065913eb2157831debcf96ad73b311316b6bd82
Lucas Alves <lucashalves@live.com>
2018-06-18 16:26:42 -0300

    forgot my pass

--- Translate/enUS/forgotMyPassEmail.js
+++ Translate/enUS/forgotMyPassEmail.js
@@ -1,7 +1,7 @@
 module.exports = {
   forgotMyPassSubject: 'Have you forgotten your password mate!?',
   titleH1: ' Reset your password ',
-  descriptionPart1: ' Hi @{{userName}}, someone recently asked to change your password for iguess app. Copy and paste the following number at reset area ',
+  descriptionPart1: ' Hi @{{userName}}, someone recently asked to change your password for iguess app. Copy and paste the following token at reset area ',
   descriptionHighLight: '“Already have the reset token”',
   descriptionPart2: '. If you do not want to change your password, just ignore this e-mail.',
   yourTokenIs: ' Your token is: ',
