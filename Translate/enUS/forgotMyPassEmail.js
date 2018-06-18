945e2c5d289b8cb8fcfa5061e4b15a294456ff7c
Lucas Alves <lucashalves@live.com>
2018-06-18 13:44:49 -0300

    forgotMyPass changes

--- Translate/enUS/forgotMyPassEmail.js
+++ Translate/enUS/forgotMyPassEmail.js
@@ -1,9 +1,9 @@
 module.exports = {
   forgotMyPassSubject: 'Have you forgotten your password mate!?',
-  titleH1: ' Reset my password ',
-  descriptionPart1: ' You asked to redefine the password to @{{userName}}. Copy and paste the following number at reset area ',
+  titleH1: ' Reset your password ',
+  descriptionPart1: ' Hi @{{userName}}, someone recently asked to change your password for iguess app. Copy and paste the following number at reset area ',
   descriptionHighLight: '“Already have the reset token”',
-  descriptionPart2: '. If you did not ask for the reset, just ignore this e-mail.',
+  descriptionPart2: '. If you do not want to change your password, just ignore this e-mail.',
   yourTokenIs: ' Your token is: ',
   afterToken: ' Copy the token above and paste at forgot my password session on ',
   afterTokenHighLight: ' “Already have the reset token”. ',
--- Translate/ptBR/forgotMyPassEmail.js
+++ Translate/ptBR/forgotMyPassEmail.js
@@ -1,7 +1,7 @@
 module.exports = {
   forgotMyPassSubject: 'Fala ai?! Esqueceu sua senha?',
   titleH1: ' Redefinir minha senha ',
-  descriptionPart1: ' Você solicitou uma redefinição de senha para @{{userName}}. Copie e cole o número abaixo na área de “esqueci minha senha” no link ',
+  descriptionPart1: ' Você solicitou uma redefinição de senha para @{{userName}}. Copie e cole o token abaixo na área de “esqueci minha senha” no link ',
   descriptionHighLight: '“Já tenho o token para redefinição”',
   descriptionPart2: '. Se você não fez essa solicitação, ignore este e-mail.',
   yourTokenIs: ' Seu token é: ',
